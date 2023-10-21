532ec38df272fce6edaccd7b6542e213a87c5b64
plod111 <ben.cornish@gmail.com>
2023-10-20 21:39:50 -0400

    mukingAround

--- knightsTour_v2.java
+++ knightsTour_v2.java
@@ -1,30 +1,45 @@
 public class knightsTour_v2 {
 
-	static final int SIZE = 5; // makes recursion much faster
+	static final int SIZE = 6; // makes recursion much faster
 	static final int MAX_MOVES = SIZE * SIZE;
 	static int[][] board = new int[SIZE][SIZE];
 	static boolean finished = false;
 	static int[] rowMoves = { +2, +2, -2, -2, +1, +1, -1, -1 };
 	static int[] colMoves = { +1, -1, +1, -1, +2, -2, +2, -2 };
 
+	static int startRow = 0;
+	static int startCol = 0;
+
+	static int attemptedMoves = 0;
+
 	public static void knightsTour(int move, int row, int col) {
+
+		attemptedMoves++;
+		// if(attemptedMoves%1000000==0){
+		// 	System.out.println("Attempted Moves: " + attemptedMoves);
+		// }
+
 		// fell off board.
 		if (row < 0 || row >= board.length || col < 0 || col >= board[row].length)
 			return;
 
-		// already here.
-		if ((board[row][col] != 0)  && ( (move == MAX_MOVES) && (row != 2 && col != 1) || (row != 1 || col != 2)))
+		// already here. ie. been here before, don't proceed
+		if ((board[row][col] != 0) ) {
 			return;
+		}
 
-
+		if ((move == MAX_MOVES-1) && ((row != 2 && col != 1) || (row != 1 && col != 2))) {
+			// System.out.println("Not landing");
+			return;
+		}
+		
 		// mark my spot
 		board[row][col] = move;
-		//printBoard();
-		//System.out.println();
 
 		// stop the recursion -- we're done.
-		if ((move == MAX_MOVES)){
+		if ((move == MAX_MOVES)) {
 			System.out.println("Finished successfully! Move: " + move);
+			System.out.println("Attempted Moves: " + attemptedMoves);
 			finished = true;
 			return;
 		}
@@ -47,17 +62,16 @@ public class knightsTour_v2 {
 				board[i][j] = 0;
 
 		// kick off the recursion.
-		knightsTour(1, 0, 0);
+		knightsTour(1, startRow, startCol);
 
 		printBoard();
 
 	}
 
-
 	/**
 	 * Prints the board.
 	 */
-	public static void printBoard(){
+	public static void printBoard() {
 		for (int i = 0; i < board.length; i++) {
 			for (int j = 0; j < board[i].length; j++) // why not board.length?
 				System.out.print(board[i][j] + "\t");
@@ -67,8 +81,6 @@ public class knightsTour_v2 {
 	}
 
 }
-
-
 
 // public class knightsTour_v1 {
 // public static void main(String[] args) {
