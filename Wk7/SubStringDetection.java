e80187b3b2b406d08f8370d2a63d5522b66a8eda
ink7012 <pchu5@student.touro.edu>
2023-10-26 23:11:24 -0400

    Completed SubStringDetection

--- Wk7/SubStringDetection.java
+++ Wk7/SubStringDetection.java
@@ -3,15 +3,16 @@ public class SubStringDetection {
     public static boolean subStringDetect(String word, String substring, int n) {
 
         //base case if at the end of word return false
-        if (n >= word.length-substring.length) return false;
+        if (n > word.length()-substring.length()) return false;
 
 
         //base case if detecetde
-    if (word.substring(n,n+substring.length).equals(substring)) return true;
+    if (word.substring(n,n+substring.length()).equals(substring)) return true;
 
     // recurse n+1
     else
-        substring(word, substring, n+1);
+        return subStringDetect(word, substring, n+1);
+        
 
     }
 
@@ -19,9 +20,9 @@ public class SubStringDetection {
 
     public static void main(String[] args) {
 
-        String word = "abcdefghijklmnopqrstuvwxyz";
+        String word = args[0];
 
-        String substring = "efg";
+        String substring = args[1];
         
         System.out.println("Sub-string Detected: " + subStringDetect(word,substring,0));
 
