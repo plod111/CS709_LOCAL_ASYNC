ccd91605d4d3daa3b8b1929272980c61eaccc343
ink7012 <pchu5@student.touro.edu>
2023-10-29 11:26:29 -0400

    letterPermutations V2 - output unique values only

--- Wk7/letterPermutations.java
+++ Wk7/letterPermutations.java
@@ -1,9 +1,10 @@
-import java.util.Scanner;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * CS709 - Week 7 ASYNC 
  * Exercise: Generate all the permutations of 7 letters recursively
- * Version: 1
+ * Version: 2
  * 
  * @author P.Chu
  * @collaborator B.Cornish
@@ -11,29 +12,48 @@ import java.util.Scanner;
  */
 
 
-public class letterPermutations{
+public class LetterPermutations{
 
 
+    /**
+     * @param args
+     */
     
     public static void main(String[] args) {
-        if (args.length != 1 || args[0].length() != 7) {    
+
+    
+        if (args.length != 1 || args[0].length() != 7) {     // only accepts 7 letter strings or else program exits.
             System.out.println("Only seven letter strings are accepted.");
             System.exit(0);
         }
 
         String input = args[0];
         char[] inputArray = input.toCharArray(); // sets input to a char array
-        permuteString(inputArray, inputArray.length); 
+
+        Set<String> uniquePermutations = new HashSet<>();    //creates new hashset for unique permutations.
+
+        permuteString(inputArray, inputArray.length, uniquePermutations);
+
+        for (String permutations : uniquePermutations) {
+            System.out.println(permutations);
+        }
 
     }//main end
     
-    public static void permuteString(char[] input, int size) {
+    public static void permuteString(char[] input, int size, Set<String> uniquePermutations) {
+        //base case if method is working the last letter in the char array.
         if (size == 1) {
-            System.out.println(new String(input));
-        } else {
-            for (int i = 0; i < size; i++) {
-                permuteString(input, size - 1);
+            // converts char array back to a string when base case is met.
+            String permutation = new String(input); 
 
+            // adds the new string to permuteString set.
+            uniquePermutations.add(permutation);
+            
+        } else {
+            //creates a permutation recursively by swapping position of chars.
+            for (int i = 0; i < size; i++) {  
+                permuteString(input, size - 1, uniquePermutations);  
+                
                 if (size % 2 == 1) {
                     swap(input, 0, size - 1);
                 } else {
@@ -42,7 +62,7 @@ public class letterPermutations{
             }
         }
     }
-
+    //helper method to swap chars for the input array
     public static void swap(char[] array, int i, int j) {
         char temp = array[i];
         array[i] = array[j];
