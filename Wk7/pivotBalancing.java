88e9aa424930da302134c98ae742ecc06aabb2f7
plod111 <ben.cornish@gmail.com>
2023-10-29 09:06:16 -0400

    header

---
+++ Wk7/pivotBalancing.java
@@ -0,0 +1,20 @@
+/**
+ * pivotBalancing
+ * 
+ * Program that attempts to recursively load balance a very tiny one row 
+ * airplane, simulated by an 1D array of numbers entered on the
+ * command line. Here are the rules:
+ *   a. implement recursively.
+ *   b. may only swap element n in the array with element length-n-1 
+ *     (i.e. the element on the other side of the center).
+ *   c. Load is defined as the sum of the elements to the left of the center vs.
+ *     the sum of the elements to the right of the center.
+ *   d. The load on each side does not have to be exactly the same, it should,
+ *     however be as close as possible.
+ *   e. You do not need to recurse within the left or right side.
+ */
+
+
+public class pivotBalancing {
+    
+}
