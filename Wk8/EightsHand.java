94cddeeae7657339e587b815b3aac2c220e25e03
plod111 <ben.cornish@gmail.com>
2023-11-05 13:28:00 -0500

    scoreHand added

--- Wk8/EightsHand.java
+++ Wk8/EightsHand.java
@@ -7,5 +7,35 @@ public class EightsHand extends Hand {
     }
 
 
+    public int scoreHand() {
+
+        int score = 0;
+
+        for (int i = 0; i < size(); i++) {
+
+            EightsCard card = (EightsCard) getCard(i);
+
+            int rank = card.getRank();
+
+            if (rank == 8) {
+
+                score -= 20;
+
+            } else if (rank > 10) {
+
+                score -= 10;
+
+            } else {
+
+                score -= rank;
+
+            }
+
+        }
+
+        return score;
+
+    }
+
     
 }
