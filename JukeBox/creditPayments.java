c82f3b0c2d6cc82cfe50550a808f4bba9ab77765
ink7012 <pchu5@student.touro.edu>
2023-11-28 15:54:22 -0500

    Added classes.

---
+++ JukeBox/creditPayments.java
@@ -0,0 +1,18 @@
+package JukeBox;
+
+public class creditPayments implements payments{
+    
+    @Override
+    public void addFunds(int amount) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public String returnFunds() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+
+}
