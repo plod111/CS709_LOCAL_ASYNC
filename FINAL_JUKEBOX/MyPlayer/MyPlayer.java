2e64bca8b87796e5ce5d273b88f275da7a100b32
plod111 <ben.cornish@gmail.com>
2023-12-03 23:59:14 -0500

    some new music and file for one of

--- FINAL_JUKEBOX/MyPlayer/MyPlayer.java
+++ FINAL_JUKEBOX/MyPlayer/MyPlayer.java
@@ -19,87 +19,97 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 // Imports relevant to the animation effects
-import javafx.animation.Timeline; 
-import javafx.animation.ParallelTransition; 
-import javafx.animation.RotateTransition; 
-import javafx.animation.ScaleTransition; 
-import javafx.animation.TranslateTransition; 
+import javafx.animation.Timeline;
+import javafx.animation.ParallelTransition;
+import javafx.animation.RotateTransition;
+import javafx.animation.ScaleTransition;
+import javafx.animation.TranslateTransition;
+
+import java.io.File;
+
 import javafx.animation.FillTransition;
-import javafx.util.Duration; 
+import javafx.util.Duration;
 
 // Imports relevant to playing MP3 music.
 import javafx.scene.media.*;
 import javafx.scene.Node;
 
 public class MyPlayer extends Application {
- 
-   @Override
-   public void start(Stage stage) {
-	// Builds the base window for the GUI (graphical user interface).
-	Group root = new Group();
-	Scene scene = new Scene(root, 500, 500, Color.BLACK);
 
+	@Override
+	public void start(Stage stage) {
+		// Builds the base window for the GUI (graphical user interface).
+		Group root = new Group();
+		Scene scene = new Scene(root, 500, 500, Color.BLACK);
 
-	// Builds the initial rectangle that will eventually be animated. 
-	Rectangle r = new Rectangle(25,25,250,250);
-	r.setFill(Color.BLUE);
-	root.getChildren().add(r);
+		// Builds the initial rectangle that will eventually be animated.
+		Rectangle r = new Rectangle(25, 25, 250, 250);
+		r.setFill(Color.BLUE);
+		root.getChildren().add(r);
 
- 
-	// Media player code for the music.
-	String      mp3;
-	Media       med;
-	MediaPlayer mdp;
-	MediaView   mdv;
+		// Media player code for the music.
+		String mp3;
+		Media med;
+		MediaPlayer mdp;
+		MediaView mdv;
 
-
-	//mp3 = "https://....";
-	mp3 = "file///home/plod/Documents/CS-709/CS709_LOCAL_ASYNC/FINAL_JUKEBOX/bensound-clearskies.mp3";
-		// note: you can find free, open-source, legally distributable music for this project.
+		// mp3 = "https://....";
+		// mp3 = "clearskies.mp3";
+		mp3 = "file:/home/plod/Documents/CS-709/CS709_LOCAL_ASYNC/FINAL_JUKEBOX/MusicMP3/quotthis-wayquot-hip-hop-beat-113255.mp3";
+		// note: you can find free, open-source, legally distributable music for this
+		// project.
 		// Examples:
-		// 	https://pixabay.com/music/
-		// 	https://freemusicarchive.org/about/
+		// https://pixabay.com/music/
+		// https://freemusicarchive.org/about/
 
-	med = new Media       ( mp3 );
-	mdp = new MediaPlayer ( med );
-	mdv = new MediaView   ( mdp );
-	root.getChildren().add(mdv);
+		// FileChooser fc = new FileChooser();
+		// fc.setTitle("Choose a music file to play.");
+		// File file = fc.showOpenDialog(stage);
 
-
-	// Add the animation effects
-        TranslateTransition translate = new TranslateTransition(Duration.millis(750)); 
-        translate.setToX(390); 
-        translate.setToY(390); 
- 
-        FillTransition fill = new FillTransition(Duration.millis(750)); 
-        fill.setToValue(Color.RED); 
- 
-        RotateTransition rotate = new RotateTransition(Duration.millis(750)); 
-        rotate.setToAngle(360); 
- 
-        ScaleTransition scale = new ScaleTransition(Duration.millis(750)); 
-        scale.setToX(0.1); 
-        scale.setToY(0.1); 
- 
-        ParallelTransition transition = new ParallelTransition(r, 
-        translate, fill, rotate, scale); 
-        transition.setCycleCount(Timeline.INDEFINITE);
-        transition.setAutoReverse(true); 
-        transition.play(); 
+		// String URL = file.toURI().toString();
+		// System.out.println(URL);
+		// med = new Media(URL);
 
 
-	// Launch the application window
-	stage.setTitle("Music Player Demo");
-	stage.setScene(scene);
-	stage.show();
-	mdp.play();
+		med = new Media(mp3);
+		mdp = new MediaPlayer ( med );
+		mdv = new MediaView ( mdp );
+		root.getChildren().add(mdv);
 
-   }
- 
-   public static void main(String[] args) {
-       launch(args);
-   }
+		// Add the animation effects
+		TranslateTransition translate = new TranslateTransition(Duration.millis(750));
+		translate.setToX(390);
+		translate.setToY(390);
+
+		FillTransition fill = new FillTransition(Duration.millis(750));
+		fill.setToValue(Color.RED);
+
+		RotateTransition rotate = new RotateTransition(Duration.millis(750));
+		rotate.setToAngle(360);
+
+		ScaleTransition scale = new ScaleTransition(Duration.millis(750));
+		scale.setToX(0.1);
+		scale.setToY(0.1);
+
+		ParallelTransition transition = new ParallelTransition(r,
+				translate, fill, rotate, scale);
+		transition.setCycleCount(Timeline.INDEFINITE);
+		transition.setAutoReverse(true);
+		transition.play();
+
+		// Launch the application window
+		stage.setTitle("Music Player Demo");
+		stage.setScene(scene);
+		stage.show();
+		mdp.play();
+
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
