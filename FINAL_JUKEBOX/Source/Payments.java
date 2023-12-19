3c8c9894c5b60642f75d5ec147f1c8053e3608b1
plod111 <ben.cornish@gmail.com>
2023-12-19 00:03:04 -0500

    added header comments

--- FINAL_JUKEBOX/Source/Payments.java
+++ FINAL_JUKEBOX/Source/Payments.java
@@ -1,5 +1,20 @@
-// Purpose: Interface for payments
+/**
+ * Payments.java - Fall 2023
+ * 
+ * This interface class defines two minimum methods for payments
+ * 
+ * CS709 Hunter Fall 2023 - Final Project
+ * 
+ * @author P.Chu
+ * @date Dec 2023
+ * 
+ */
+
 public interface Payments {
+
+    // add funds to the currency box
     void addFunds(int amount);
+
+    // return funds from the currency box
     String returnFunds();
 }
